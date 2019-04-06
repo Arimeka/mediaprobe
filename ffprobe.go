4c31689b1d410eec86ea415ca2c6c74c5663efaa
Navi Arimeka <wrk.navi@gmail.com>
2019-04-06 20:23:17 +0300

    More data from parsing video

--- ffprobe.go
+++ ffprobe.go
@@ -23,21 +23,34 @@ func (probe *Info) FFProbe(filepath string) error {
 			return err
 		}
 
-		codec := stream.GetCodecPar()
+		codecCtx := stream.CodecCtx()
+		codec := codecCtx.Codec()
 
 		streamInfo := Stream{
-			ID:      stream.Id(),
-			Index:   stream.Index(),
-			Bitrate: codec.GetBitRate(),
+			ID:             stream.Id(),
+			Index:          stream.Index(),
+			Bitrate:        codecCtx.BitRate(),
+			MediaType:      codecCtx.GetMediaType(),
+			Codec:          codec.Name(),
+			CodecLongName:  codec.LongName(),
+			CodecTag:       codecCtx.GetCodecTagName(),
+			IsExperimental: codec.IsExperimental(),
+			SampleFmtName:  codecCtx.GetSampleFmtName(),
 		}
 
 		if stream.IsVideo() {
-			streamInfo.Width = codec.GetWidth()
-			streamInfo.Height = codec.GetHeight()
+			streamInfo.Width = codecCtx.Width()
+			streamInfo.Height = codecCtx.Height()
 			streamInfo.FrameRate = stream.GetRFrameRate().AVR().Av2qd()
 			streamInfo.AvgFrameRate = stream.GetAvgFrameRate().AVR().Av2qd()
+			streamInfo.Profile = codecCtx.GetProfileName()
+			streamInfo.ColorRangeName = codecCtx.GetColorRangeName()
+			streamInfo.AspectRation = codecCtx.GetAspectRation().AVR().Av2qd()
+			streamInfo.BFrames = codecCtx.GetBFrames()
+			streamInfo.BitsPerSample = codecCtx.GetBitsPerSample()
 		}
 
+		codecCtx.Free()
 		stream.Free()
 
 		probe.Streams = append(probe.Streams, streamInfo)
--- info.go
+++ info.go
@@ -16,13 +16,22 @@ type Info struct {
 }
 
 type Stream struct {
-	ID           int
-	Index        int
-	MediaType    string
-	Codec        string
-	Bitrate      int64
-	Width        int
-	Height       int
-	FrameRate    float64
-	AvgFrameRate float64
+	ID             int
+	Index          int
+	MediaType      string
+	Codec          string
+	CodecTag       string
+	CodecLongName  string
+	IsExperimental bool
+	Profile        string
+	ColorRangeName string
+	SampleFmtName  string
+	Bitrate        int
+	Width          int
+	Height         int
+	AspectRation   float64
+	FrameRate      float64
+	AvgFrameRate   float64
+	BFrames        int
+	BitsPerSample  int
 }
